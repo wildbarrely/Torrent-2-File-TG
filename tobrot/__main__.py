50545fa194986c65b2eb6652ad20ac99814959f3
rakul <rakul0agn@gmail.com>
2020-06-17 17:24:37 +0530

    Update __main__.py

--- tobrot/__main__.py
+++ tobrot/__main__.py
@@ -54,7 +54,7 @@ if __name__ == "__main__" :
     #
     incoming_message_handler = MessageHandler(
         incoming_message_f,
-        filters=Filters.command(["download@TorrenFileLeech_bot"]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=Filters.command(["Download@TorrenFileLeech_bot"]) & Filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(incoming_message_handler)
     #
@@ -78,7 +78,7 @@ if __name__ == "__main__" :
     #
     cancel_message_handler = MessageHandler(
         cancel_message_f,
-        filters=Filters.command(["cancel@TorrenFileLeech_bot"]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=Filters.command(["cancel"]) & Filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(cancel_message_handler)
     #
